86ef07a79a579f4fd7463dfd0605717d69e6c00c
EshanJayasundara <e19163@eng.pdn.ac.lk>
2025-05-10 12:02:19 +0530

    debug

--- django_project/settings.py
+++ django_project/settings.py
@@ -11,7 +11,7 @@ https://docs.djangoproject.com/en/5.2/ref/settings/
 """
 
 from pathlib import Path
-import os
+from decouple import config
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -21,8 +21,8 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/5.2/howto/deployment/checklist/
 
 # SECURITY WARNING: don't run with debug turned on in production!
-SECRET_KEY = os.environ['DJANGO_SECRET_KEY']
-DEBUG = os.environ['DEBUG']
+SECRET_KEY = config('DJANGO_SECRET_KEY')
+DEBUG = config('DEBUG', cast=bool, default=False)
 
 
 ALLOWED_HOSTS = []
