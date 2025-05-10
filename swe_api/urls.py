98ab33309a49143da6a751136455a597030d8f5e
EshanJayasundara <e19163@eng.pdn.ac.lk>
2025-05-10 14:35:29 +0530

    removed custom api-key-generate endpoint

--- swe_api/urls.py
+++ swe_api/urls.py
@@ -1,5 +1,5 @@
 from django.urls import path
-from .views import SampleAPIView, GenerateApiKeyView
+from .views import SampleAPIView
 
 urlpatterns = [
     path('swe_eval/', SampleAPIView.as_view()),       # GET all, POST create
