0d439ba77b2053054602e2895a059063ff6821ff
EshanJayasundara <e19163@eng.pdn.ac.lk>
2025-07-08 00:57:47 +0530

    done

--- swe_api/urls.py
+++ swe_api/urls.py
@@ -1,7 +1,8 @@
 from django.urls import path
-from .views import SampleAPIView, SweAPIView
+from .views import SampleAPIView, SweAPIView, FileUploadView
 
 urlpatterns = [
     path('test/', SampleAPIView.as_view(), name='test'),
     path('swe_eval/', SweAPIView.as_view()),
+    path('file_upload/', FileUploadView.as_view()),
 ]
