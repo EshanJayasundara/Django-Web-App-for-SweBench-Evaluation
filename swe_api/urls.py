12ad0bffae6f2b00e45fa777c36c7e171e5337e5
EshanJayasundara <e19163@eng.pdn.ac.lk>
2025-07-08 01:15:24 +0530

    done

--- swe_api/urls.py
+++ swe_api/urls.py
@@ -3,6 +3,6 @@ from .views import SampleAPIView, SweAPIView, FileUploadView
 
 urlpatterns = [
     path('test/', SampleAPIView.as_view(), name='test'),
-    path('swe_eval/', SweAPIView.as_view()),
-    path('file_upload/', FileUploadView.as_view()),
+    path('api/swe_eval/', SweAPIView.as_view(), name='swe_eval'),
+    path('api/file_upload/', FileUploadView.as_view(), name='file_upload'),,
 ]
