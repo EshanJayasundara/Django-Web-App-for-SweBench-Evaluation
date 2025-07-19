760f111cb721b09fb41cfc4d701ee857dab184a4
EshanJayasundara <e19163@eng.pdn.ac.lk>
2025-07-19 22:43:43 +0530

    model_name_or_path updated

--- swe_api/views.py
+++ swe_api/views.py
@@ -175,8 +175,10 @@ class SweAPIView(APIView):
                         prediction["model_patch"] = prediction.pop("patch")
 
                     if "model_name_or_path" not in prediction:
-                        if prediction["model_name_or_path"] != "debugai":
-                            prediction["model_name_or_path"] = "debugai"
+                        prediction["model_name_or_path"] = "debugai"
+                    elif prediction["model_name_or_path"] != "debugai":
+                        prediction["model_name_or_path"] = "debugai"
+                        
 
                     required_keys = {"instance_id", "model_name_or_path", "model_patch"}
                     if set(prediction.keys()) != required_keys:
